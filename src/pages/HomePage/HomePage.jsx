8a3c0a94184829a8409a9945bd18a31f72f05b44
chasov-artem <chasov90@gmail.com>
2025-01-29 11:51:45 +0200

    HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,22 @@
+import { Link } from "react-router-dom";
+import s from "./HomePage.module.css";
 const HomePage = () => {
-  return <h1>Welcome to Travel Tracks! 🚐</h1>;
+  return (
+    <>
+      <div className={s.container}>
+        <div className={s.contentWrapper}>
+          <div className={s.titleWrapper}>
+            <h1 className={s.mainTitle}>Campers of your dreams</h1>
+            <h2 className={s.title}>
+              You can find everything you want in our catalog
+            </h2>
+          </div>
+          <Link to={"/catalog"} className={s.link}>
+            View Now
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 export default HomePage;
