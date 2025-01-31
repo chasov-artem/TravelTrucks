9d7704522b19901b7126dfb3520e67d17d7daacc
chasov-artem <chasov90@gmail.com>
2025-01-31 14:37:40 +0200

    Review/styles

--- src/components/ReviewsSection/ReviewsSection.jsx
+++ src/components/ReviewsSection/ReviewsSection.jsx
@@ -3,12 +3,19 @@ import s from "./Reviews.module.css";
 const ReviewsSection = ({ reviews }) => {
   return (
     <div className={s.reviews}>
-      <h3>Customer Reviews</h3>
       {reviews.length > 0 ? (
         reviews.map((review, index) => (
           <div key={review.id || `review-${index}`} className={s.review}>
-            <strong>{review.reviewer_name}</strong>
-            <p>{review.comment}</p>
+            <div className={s.avatarRating}>
+              <div className={s.avatar}>
+                <span>{review.reviewer_name[0]}</span>
+              </div>
+              <div className={s.content}>
+                <strong className={s.name}>{review.reviewer_name}</strong>
+                <p className={s.rating}>Rating: {review.reviewer_rating} / 5</p>
+              </div>
+            </div>
+            <p className={s.comment}>{review.comment}</p>
           </div>
         ))
       ) : (
