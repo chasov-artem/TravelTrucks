368f4217c5001a3850e81ca0fae25d822817fa87
chasov-artem <chasov90@gmail.com>
2025-02-01 11:20:24 +0200

    Pagination/LoadMore

--- src/components/LoadMoreButton/LoadMoreButton.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -1,10 +1,11 @@
 import s from "./LoadMoreButton.module.css";
 
-const LoadMoreButton = ({ onClick, isLoading }) => {
+const LoadMoreButton = ({ onClick }) => {
   return (
-    <button onClick={onClick} className={s.loadMore} disabled={isLoading}>
-      LoadMore
+    <button onClick={onClick} className={s.loadMore}>
+      Load More
     </button>
   );
 };
+
 export default LoadMoreButton;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -2,11 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import {
   fetchCampers,
-  selectCampers,
   selectIsLoading,
   selectError,
 } from "../../redux/campers/campersSlice";
-import FilterBar from "../../components/FIlterBar/FilterBar";
+import FilterBar from "../../components/FilterBar/FilterBar";
 import LoadMoreButton from "../../components/LoadMoreButton/LoadMoreButton";
 import s from "./CatalogPage.module.css";
 import { CamperList } from "../../components/CamperList/CamperList";
@@ -15,26 +14,43 @@ import Loader from "../../components/Loader/Loader";
 
 const CatalogPage = () => {
   const dispatch = useDispatch();
-  const campers = useSelector(selectCampers);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   const filters = useSelector(selectFilters);
 
   const [page, setPage] = useState(1);
+  const [allCampers, setAllCampers] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    setAllCampers([]);
+    setPage(1);
+    setHasMore(true);
+  }, [filters]);
 
   useEffect(() => {
     const formattedFilters = {
-      page: page || 1,
+      page,
       limit: 3,
       location: filters.location || undefined,
       type: filters.type || undefined,
       amenities: filters.amenities || [],
     };
 
-    dispatch(fetchCampers(formattedFilters));
+    dispatch(fetchCampers(formattedFilters)).then((response) => {
+      if (page === 1) {
+        setAllCampers(response.payload);
+      } else {
+        setAllCampers((prev) => [...prev, ...response.payload]);
+      }
+
+      if (response.payload.length === 0) {
+        setHasMore(false);
+      }
+    });
   }, [dispatch, page, filters]);
 
-  if (isLoading) {
+  if (isLoading && page === 1) {
     return <Loader />;
   }
 
@@ -46,8 +62,8 @@ const CatalogPage = () => {
     <div className={s.catalog}>
       <FilterBar />
       <div className={s.listBtn}>
-        <CamperList campers={campers} />
-        {!isLoading && Array.isArray(campers) && campers.length > 0 && (
+        <CamperList campers={allCampers} />
+        {!isLoading && hasMore && (
           <LoadMoreButton onClick={() => setPage((prev) => prev + 1)} />
         )}
       </div>
