b214ab915df3e273d12ea0e5e64c4997173f9354
chasov-artem <chasov90@gmail.com>
2025-02-10 09:12:32 +0200

    styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 import App from "./App";
 import "./index.css";
+import "modern-normalize";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
